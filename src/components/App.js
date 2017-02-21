3b11fcbb2cc7d0b6332c6a58911b0f20bc7417b5
xCrystal <rgr.crystal@gmail.com>
2017-02-21 08:41:22 +0000

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header"></div>
+        <div className="App-intro"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
