7ae72842b3f4c25c742e12e5757780d28c49422f
Jorge <jorgesasiain@gmail.com>
2017-03-10 17:38:47 +0000

    trying to login with strophe

--- src/engine/xmppLogin.js
+++ src/engine/xmppLogin.js
@@ -1,5 +1,11 @@
+var strophe = require("./strophe/strophe.js");
+var Strophe = strophe.Strophe;
+
 module.exports = {
 	auth: function(jid, pass) {
-		alert(jid + ' ' + pass);
+		var conn = new Strophe.Connection('https://bind.jappix.com:5280/xmpp-httpbind/');
+		conn.connect(jid, pass, function (status) {
+			alert(status);
+		});
 	},
 };
