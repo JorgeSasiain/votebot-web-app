faa6cc5f15987e7119453c19ed0ee0eac6fae5c3
Jorge <jorgesasiain@gmail.com>
2017-03-11 00:27:50 +0000

    connect to bosh server and import strophe from modules

--- src/engine/xmppLogin.js
+++ src/engine/xmppLogin.js
@@ -1,11 +1,25 @@
-var strophe = require("./strophe/strophe.js");
+import strophe from 'strophe.js';
 var Strophe = strophe.Strophe;
 
 module.exports = {
-	auth: function(jid, pass) {
-		var conn = new Strophe.Connection('https://bind.jappix.com:5280/xmpp-httpbind/');
-		conn.connect(jid, pass, function (status) {
-			alert(status);
-		});
-	},
+  auth: function(jid, pass) {
+    var conn = new Strophe.Connection("http://bosh.metajack.im:5280/xmpp-httpbind");
+    conn.connect(jid, pass, function (status) {
+      if (status === Strophe.Status.CONNECTED) {
+        alert('connected');
+      } else if (status === Strophe.Status.DISCONNECTED) {
+        alert('disconnected');
+      } else if (status === Strophe.Status.CONNECTING) {
+        alert('connecting');
+      } else if (status === Strophe.Status.DISCONNECTING) {
+        alert('disconnecting');
+      } else if (status === Strophe.Status.AUTHENTICATING) {
+        alert('authenticating');
+      } else if (status === Strophe.Status.CONNFAIL) {
+        alert('connfail');
+      } else if (status === Strophe.Status.AUTHFAIL) {
+        alert('authfail');
+      }
+    });
+  },
 };
