dba84dcb081f9a673cf982cd85671fa168f38da7
Jorge <jorgesasiain@gmail.com>
2017-05-10 19:23:32 +0100

    read mongodb uri from heroku config vars

--- src/server/mongo.js
+++ src/server/mongo.js
@@ -1,14 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
-import { MONGO_URI } from './mongoCredentials.js';
 
 const Mongo = {
 
-  MONGO_URI: MONGO_URI,
+  MONGO_URI: process.env.MONGO_URI,
 
   db: {},
 
   connect: function() {
-    Mongo.db = MongoClient.connect(MONGO_URI, function(err, db) {
+    Mongo.db = MongoClient.connect(Mongo.MONGO_URI, function(err, db) {
       if (err !== null) alert("err");
       if (db === null) alert("db-null");
       Mongo.db = db;
